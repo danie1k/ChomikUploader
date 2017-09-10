8a947af546caa5052618d500d75ebd4184ef604d
Daniel Kuruc <dnk@dnk.net.pl>
2017-09-10 22:40:04 +0200

    Update 'setup.py'

--- setup.py
+++ setup.py
@@ -1,28 +1,37 @@
-from distutils.core import setup
 import sys
-import os
+from distutils.core import setup
 
 if sys.platform.startswith('win'):
     import py2exe
-    
-setup(name='chomikUploader',
-          version='0.5.4.3',
-          author='adam_gr',
-          author_email='adam_gr [at] gazeta.pl',
-          description='Uploading files on chomikuj.pl',
-          package_dir = {'chomikuploader' : 'src'},
-          packages = ['chomikuploader'],
-          options = {"py2exe" : {
-                                  "compressed" : True,
-                                  "ignores" : ["email.Iterators", "email.Generator"],
-                                  "bundle_files" : 1
-                                },
-                     "sdist"  : {
-                                  'formats': 'zip'
-                                }
-                    },
-          scripts = ['chomik'],
-          console = ['chomik'],
-          zipfile = None
-         )
 
+AUTHOR = __import__('src').__author__
+EMAIL = __import__('src').__email__
+LICENSE = __import__('src').__license__
+VERSION = __import__('src').__version__
+
+setup(
+    name='ChomikUploader',
+    version=VERSION,
+    author=AUTHOR,
+    author_email='dnk@dnk.net.pl',
+    license=LICENSE,
+    description='Uploading files on Chomikuj.pl',
+    package_dir={'chomikuploader': 'src'},
+    packages=['chomikuploader'],
+    options={
+        'py2exe': {
+            'compressed': True,
+            'ignores': ['email.Iterators', 'email.Generator'],
+            'bundle_files': 1
+        },
+        'sdist': {
+            'formats': 'zip'
+        }
+    },
+    scripts=['chomik'],
+    console=['chomik'],
+    zipfile=None,
+    install_requires=[
+        'xmltodict>=0.11.0'
+    ]
+)
